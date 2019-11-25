b520c03711b0752bb93294149c39893fcd57a847
Mordred-x <maurofuentes@msn.com>
2019-11-25 20:20:31 +0100

    starter

---
+++ How to make a ball roll in Unity 3D Starter/Assets/Scenes/RW/Scripts/MoveBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    Rigidbody rigidBodyFromBall;
+
+    private void Start()
+    {
+        rigidBodyFromBall = GetComponent<Rigidbody>();
+        rigidBodyFromBall.maxAngularVelocity = 15;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // 1
+
+        Vector3 xMovement = new Vector3(moveHorizontal, 0, 0); // 2
+
+        rigidBodyFromBall.AddForce(xMovement); // 3
+    }
+}
---
+++ How to make a ball roll in Unity 3D Starter/Assets/Scenes/RW/Scripts/SuccessTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SuccessTrigger : MonoBehaviour
+{
+    public GameObject text; // 1 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        text.SetActive(true); // 2 
+    }
+}
